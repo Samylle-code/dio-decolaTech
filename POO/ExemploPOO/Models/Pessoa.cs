635ddd21ec96efba391b5e3a99cbfc608fbccd04
Samylle <samyllecaetano@hotmail.com>
2022-01-21 09:54:39 -0300

    Reorganização de pastas

---
+++ POO/ExemploPOO/Models/Pessoa.cs
@@ -0,0 +1,18 @@
+﻿
+
+
+using System;
+
+
+namespace EstudoObjeto{
+
+	public class Pessoa
+	{
+		public string nome {get;set;}
+		public int Idade { get; set; }
+		public void Apresentar()
+        {
+			Console.WriteLine($"Olá meu nome é {nome} e tenho {Idade} anos.");
+        }
+	}
+}
